b9a140bc84a9e5aa31cb187f17b4e26945691c65
Igor Mayachenkov <igor.mayachenkov@gmail.com>
2020-11-18 22:53:03 +0300

    PWM on timer/counter1 implemented

--- main.cpp
+++ main.cpp
@@ -13,9 +13,12 @@
 #define SetPin(port,pin)    port|=(1<<pin)
 #define ClearPin(port,pin)  port&=~(1<<pin)
 
+// PWM
+#define PWM_PIN 1 //PB1/OC1A
+unsigned char valPWM = 6;
 
 // Debug LED - primitive UI
-#define Debug_PIN  3
+#define Debug_PIN  4
 bool debugLed = true;
 void applyDebugLed(){
 	if(debugLed)
@@ -28,7 +31,7 @@ void applyDebugLed(){
 int main(void)
 {
 	// Initialize PORTs
-	DDRB  = (1<<Debug_PIN); // Debug pin under control
+	DDRB  = 1<<Debug_PIN | 1<<PWM_PIN; // Debug pin under control
 	//PORTB = 0; // Initial 0
 
 	applyDebugLed();
@@ -43,8 +46,13 @@ int main(void)
 	TCCR0B = 1<<CS02 | 0<<CS01 | 1<<CS00; // Internal Clock/1024
 	
 	// PWM GENERATOR : 8-bit TIMER/COUNTER1
-
-	
+	OCR1A = 18; //(MIN)6:432us 15:1.0us   23:1480us   31:1980us(MAX)  18-middle
+	OCR1C = 0xFF;
+	// TCCR1 – Timer/Counter1 Control Register
+	TCCR1 = 1<<PWM1A // Pulse Width Modulator A Enable
+	        | 1<<COM1A1 | 0<<COM1A0 // OC1A cleared on compare match. Set when TCNT1 = $00.
+	        | 0<<CS13 | 1<<CS12 | 1<<CS11 | 1<<CS10; // Freq 63 Hz (period: 17ms)	
+			
 	// Timer/Counter Interrupt Mask Register - TIMSK
 	TIMSK = 1<<TOIE0; // Overflow interrupt enable	
 	
@@ -66,6 +74,9 @@ ISR(TIMER0_OVF_vect){
 	//if(countSkip>0) countSkip--;
 	//else{
 		//countSkip = COUNT_SKIP;
+		
+		//OCR1A = valPWM;
+		//valPWM++;
 		
 		// Toggle debug led
 		debugLed = !debugLed;
